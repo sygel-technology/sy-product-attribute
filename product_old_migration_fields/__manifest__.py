9605683377aec7a3396880b042e37de8a05cb6cd
angel <angel.garcia@sygel.com>
2023-09-21 16:35:29 +0200

    [NEW] product_old_migration_fields: Saves and displays product fields pre-migration.

---
+++ product_old_migration_fields/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Ángel García de la Chica <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Old Migration Fields",
+    "summary": "Product Old Migration Fields",
+    "version": "16.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'product'
+    ],   
+    "data": [
+        "views/product_views.xml",
+    ],
+}
