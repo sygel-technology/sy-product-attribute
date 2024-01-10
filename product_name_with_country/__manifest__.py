b76e6d3d984e04c3935cc8fe95edcbfff4d863b0
angel <angel.garcia@sygel.com>
2024-01-10 15:24:12 +0100

    [14.0][FIX] fix _name_search().

--- product_name_with_country/__manifest__.py
+++ product_name_with_country/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Name With Country",
     "summary": "Product Name With Country",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Custom",
     "website": "https://www.sygel.es",
     "author": "Sygel, Odoo Community Association (OCA)",
