b73efed9e8f2318775ce752a6f5b4869e510111e
Alberto Martínez <alberto.martinez@sygel.es>
2024-02-14 14:01:53 +0100

    T-5627[MIG][17.0]product_old_migration_fields

--- product_old_migration_fields/__manifest__.py
+++ product_old_migration_fields/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Old Migration Fields",
     "summary": "Product Old Migration Fields",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.1",
     "category": "Inventory",
     "website": "https://www.sygel.es",
     "author": "Sygel, Odoo Community Association (OCA)",
@@ -14,7 +14,7 @@
     "depends": [
         'product',
         'base_old_migration_fields'
-    ],   
+    ],
     "data": [
         "views/product_views.xml",
         "views/product_category_views.xml"
