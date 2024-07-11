a6059fcdd60820ed12878dcb150e53b666a6a88c
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 10:09:35 +0200

    [T-5944][IMP] product_name_with_attributes,product_old_migration_fields:pre-commit auto fixes

--- product_name_with_attributes/models/product_attribute.py
+++ product_name_with_attributes/models/product_attribute.py
@@ -5,8 +5,10 @@ from odoo import models
 
 
 class ProductTemplateAttributeValue(models.Model):
-    _inherit = 'product.template.attribute.value'
+    _inherit = "product.template.attribute.value"
 
     def _get_combination_name(self):
         ptavs = self._without_no_variant_attributes().with_prefetch(self._prefetch_ids)
-        return ", ".join([(ptav.attribute_id.name+": "+ptav.name) for ptav in ptavs])
+        return ", ".join(
+            [(ptav.attribute_id.name + ": " + ptav.name) for ptav in ptavs]
+        )
--- product_old_migration_fields/models/product.py
+++ product_old_migration_fields/models/product.py
@@ -1,14 +1,14 @@
 # Copyright 2023 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     old_product_id = fields.Integer(
-        string='Old Product ID',
+        string="Old Product ID",
     )
 
 
@@ -16,5 +16,5 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     old_product_template_id = fields.Integer(
-        string='Old Product Template ID',
+        string="Old Product Template ID",
     )
