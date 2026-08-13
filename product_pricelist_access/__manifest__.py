87bc511d5431b59ec49832b57db8956e841b2611
Angel Rivas <angel.rivas@sygel.es>
2026-08-13 15:12:28 +0200

    [T-9810][MIG] product_pricelist_access: Migration to 18.0

--- product_pricelist_access/__manifest__.py
+++ product_pricelist_access/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Pricelist Access",
     "summary": "Restrict access to product pricelists by user or group",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Sygel,Odoo Community Association (OCA)",
     "website": "https://github.com/sygel-technology/sy-product-attribute",
